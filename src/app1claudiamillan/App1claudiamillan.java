b17417149f0cb7f07ca347dfc34c292423228a4e
claudiamillan <millanc569@gmail.com>
2017-10-25 21:09:36 -0300

    Trabajando en clases

---
+++ src/app1claudiamillan/App1claudiamillan.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app1claudiamillan;
+
+/**
+ *
+ * @author lab12
+ */
+public class App1claudiamillan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        int numero;//Declaracion de variable
+        int numero6;
+        numero=30;
+        numero6=20;
+        
+        System.out.println("Suma de numero y numero6: "+(numero+numero6));
+        System.out.println("Resta de numero y numero6: "+(numero+numero6));
+        System.out.println("Mutiplicacion de numero y numero6: "+(numero+numero6));
+        System.out.println("Division de numero y numero6: "+(numero+numero6));
+        System.out.println("Resta del numero y numero6% (numero%numero6");
+               
+                
+         
+        // TODO code application logic here
+    }
+    
+}
